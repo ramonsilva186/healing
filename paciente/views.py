38a661323280d64614eb07115c8d26909b71d078
ramonsilva186 <ramonsilva186@gmail.com>
2024-04-20 20:14:29 -0300

    add filter listing

--- paciente/views.py
+++ paciente/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from medico.models import DadosMedicos
+from medico.models import DadosMedicos, Especialidades
 
 # Create your views here.
 def home(request):
 
     if request.method == 'GET':
+
+        medico_filtrar = request.GET.get('medico')
+        especialidade_filtrar = request.GET.getlist('especialidade')
         medicos = DadosMedicos.objects.all()
-        return render(request, 'home.html', {'medicos': medicos})
+
+        if medico_filtrar:
+            medicos = medicos.filter(nome__icontains=medico_filtrar)
+
+        if especialidade_filtrar:
+            medicos = medicos.filter(especialidade__in=especialidade_filtrar)
+
+        especialidades = Especialidades.objects.all() 
+        return render(request, 'home.html', {'medicos': medicos, 'especialidades': especialidades})
