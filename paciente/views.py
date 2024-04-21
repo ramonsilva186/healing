fa21eb6e0f5dfd3f8dff8e6a8162034d6f684aee
ramonsilva186 <ramonsilva186@gmail.com>
2024-04-21 13:19:06 -0300

    create agendamento

--- paciente/views.py
+++ paciente/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from medico.models import DadosMedicos, Especialidades, DatasAbertas
 from django.http import HttpResponse
 from datetime import datetime
+from .models import Consulta
+from django.contrib import messages
+from django.contrib.messages import constants
 
 # Create your views here.
 def home(request):
@@ -26,3 +29,24 @@ def escolher_horario(request, id_dados_medicos):
         medico = DadosMedicos.objects.get(id=id_dados_medicos)
         datas_abertas = DatasAbertas.objects.filter(user=medico.user).filter(data__gte=datetime.now()).filter(agendado=False)
         return render(request, 'escolher_horario.html', {'medico': medico, 'datas_abertas': datas_abertas})
+
+def agendar_horario(request, id_data_aberta):
+    
+    if request.method == 'GET':
+        data_aberta = DatasAbertas.objects.get(id=id_data_aberta)
+        horario_agendado = Consulta(
+            paciente=request.user,
+            data_aberta=data_aberta
+        )
+        horario_agendado.save()
+        data_aberta.agendado = True
+        data_aberta.save()
+        messages.add_message(request, constants.SUCCESS, 'Agendado com sucesso')
+
+        return redirect('/pacientes/minhas_consultas/')
+
+    if request.method == 'POST':
+        data_aberta = DatasAbertas.objects.get(id=id_data_aberta)
+        data_aberta.agendado = True
+        data_aberta.save()
+        return HttpResponse('Agendado com sucesso')
